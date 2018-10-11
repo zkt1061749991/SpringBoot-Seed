6560897fb2f04887bdb85f64985d441cb122af5e
志轩 陈 <544493924@qq.com>
2018-10-11 13:38:39 +0800

    update

---
+++ src/main/java/com/company/project/service/AnswerService.java
@@ -0,0 +1,12 @@
+package com.company.project.service;
+
+import com.company.project.model.Answer;
+import com.company.project.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2018/10/11.
+ */
+public interface AnswerService extends Service<Answer> {
+
+}
---
+++ src/main/java/com/company/project/service/impl/AnswerServiceImpl.java
@@ -0,0 +1,22 @@
+package com.company.project.service.impl;
+
+import com.company.project.dao.AnswerMapper;
+import com.company.project.model.Answer;
+import com.company.project.service.AnswerService;
+import com.company.project.core.AbstractService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+
+/**
+ * Created by CodeGenerator on 2018/10/11.
+ */
+@Service
+@Transactional
+public class AnswerServiceImpl extends AbstractService<Answer> implements AnswerService {
+    @Resource
+    private AnswerMapper answerMapper;
+
+}
