b5b8ccaacf3891358047e5a576b11bb897fb5ec9
志轩 陈 <544493924@qq.com>
2018-10-11 22:57:37 +0800

    update

--- src/main/java/com/xuanxuan/csu/dao/AskMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xuanxuan.csu.dao;
-
-import com.xuanxuan.csu.core.Mapper;
-import com.xuanxuan.csu.model.Ask;
-
-public interface AskMapper extends Mapper<Ask> {
-}
--- src/main/java/com/xuanxuan/csu/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xuanxuan.csu.dao;
-
-import com.xuanxuan.csu.core.Mapper;
-import com.xuanxuan.csu.model.User;
-
-public interface UserMapper extends Mapper<User> {
-}
